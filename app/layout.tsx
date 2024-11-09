31e328cd721d6a5ef8548a08f005c384b27a1fc3
Paulo Araújo <pauloaraujo.phz@gmail.com>
2024-11-09 20:47:19 -0300

    chore: add shadcn setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={`${inter.className}`}>{children}</body>
+      <body className={`${inter.className} dark antialiased`}>{children}</body>
     </html>
   );
 }
